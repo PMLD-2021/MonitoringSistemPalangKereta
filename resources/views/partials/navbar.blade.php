e39214ddee9506fd92be77b1743b9c16593cae61
altaufiqqi <muhammad21taufiq21@mail.ugm.ac.id>
2021-12-03 08:21:48 +0700

    konfigurasi logout

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,7 +5,10 @@
                 <a class="nav-link active text-white" href="/notification"><i class="fas fa-bell fa-lg"></i></a>
             </li>
             <li class="nav-item p-1">
-                <a class="nav-link active text-white" href="/"><i class="fas fa-door-open fa-lg"></i></a>
+                <form action="/logout" method="post">
+                    @csrf
+                    <button class="btn text-white"><i class="fas fa-door-open fa-lg"></i></button>
+                </form>
             </li>
         </ul>
     </div>
